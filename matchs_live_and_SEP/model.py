280a99f6ad9e618151febd22567262814f580781
Ahme460 <ahmeoon1234@gmail.com>
2025-01-29 00:54:10 +0200

    28/1

---
+++ matchs_live_and_SEP/model.py
@@ -0,0 +1,50 @@
+from leagues.model import Player,Player_stitic
+from dataclasses import dataclass
+from typing import List ,Dict
+
+
+@dataclass
+class StiticsMathcs:
+    shots_on_goal: int
+    shots_off_goal: int
+    shots_inside_box: int
+    shots_outside_box: int
+    total_shots: int
+    blocked_shots: int
+    fouls: int
+    corner_kicks: int
+    offsides: int
+    ball_possession: float
+    yellow_cards: int
+    red_cards: int
+    goalkeeper_saves: int
+    total_passes: int
+    passes_accurate: int
+    passes_percentage: float
+    
+
+
+
+@dataclass    
+class Info_mathc:
+    time:str
+    stadium:str
+    city:str
+    referee:str
+    
+    
+    
+    
+@dataclass    
+class Event:
+    time:int
+    player_id:int
+    player_name:str
+    event_type:str
+    detal_event:str
+    
+
+@dataclass   
+class Player_stitic_mathc(Player_stitic):
+    rating:float
+    
